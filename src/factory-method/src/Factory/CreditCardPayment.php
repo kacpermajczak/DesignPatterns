19cb0626043c6c3e03bb1e0a44ad1e52f2844abb
Kacper Majczak <k.majczak@insee.pl>
2020-09-03 15:25:44 +0200

    Add CreditCardPayment class

---
+++ src/factory-method/src/Factory/CreditCardPayment.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Kacper Majczak
+ * @site http://kacpermajczak.pl/
+ * @date 03/09/2020
+ */
+
+namespace DesignPatterns\FactoryMethod\Factory;
+
+class CreditCardPayment
+{
+
+    /**
+     * CreditCardPayment constructor.
+     */
+    public function __construct()
+    {
+    }
+}
