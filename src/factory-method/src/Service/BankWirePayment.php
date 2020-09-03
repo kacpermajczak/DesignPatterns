fe4eab8f5ff724c00d6c9a014ee31c7824c9dc5a
Kacper Majczak <k.majczak@insee.pl>
2020-09-03 16:55:10 +0200

    Add PaymentMethod interaface and implement it to services

--- src/factory-method/src/Service/BankWirePayment.php
+++ src/factory-method/src/Service/BankWirePayment.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 namespace DesignPatterns\FactoryMethod\Service;
 
 
-class BankWirePayment
+class BankWirePayment implements PaymentMethod
 {
 
 }
--- src/factory-method/src/Service/PaymentMethod.php
+++ src/factory-method/src/Service/PaymentMethod.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 declare(strict_types=1);
 /**
  * @author Kacper Majczak
@@ -8,7 +8,6 @@ declare(strict_types=1);
 
 namespace DesignPatterns\FactoryMethod\Service;
 
-class CreditCardPayment implements PaymentMethod
+interface PaymentMethod
 {
-
 }
