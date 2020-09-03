4b7841a06acbc185c40f153959ce6de02507bcf1
Kacper Majczak <k.majczak@insee.pl>
2020-09-03 15:59:21 +0200

    Add CreditCardPayment factory

---
+++ src/factory-method/src/Factory/CreditCardPaymentFactory.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Kacper Majczak
+ * @site http://kacpermajczak.pl/
+ * @date 03/09/2020
+ */
+
+
+namespace DesignPatterns\FactoryMethod\Factory;
+
+
+use DesignPatterns\FactoryMethod\Service\CreditCardPayment;
+
+class CreditCardPaymentFactory
+{
+    public function createPayment(): CreditCardPayment
+    {
+        return new CreditCardPayment();
+    }
+}
